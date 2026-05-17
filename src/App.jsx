edfc27c7eb5716cb66e97d5fba2ded0230f9d055
Bryandev2025 <kingvcuts@gmail.com>
2026-05-17 14:02:58 +0800

    feat: add App component with routing and layout

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import NotFound from './pages/NotFound.jsx'
+
+function App() {
+  return (
+    <div className="app-root">
+      <Header />
+      <main id="main">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
