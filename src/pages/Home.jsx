b28f798e381fbc33182d055b2936dba5d153c626
Bryandev2025 <kingvcuts@gmail.com>
2026-05-17 14:02:52 +0800

    feat: add Home page composed of Hero, Features and ContactForm

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from '../components/Hero.jsx'
+import Features from '../components/Features.jsx'
+import ContactForm from '../components/ContactForm.jsx'
+
+export default function Home() {
+  return (
+    <div>
+      <Hero />
+      <Features />
+      <ContactForm />
+    </div>
+  )
+}
